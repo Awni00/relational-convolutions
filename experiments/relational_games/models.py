b03015ae20652f28e680407bf1d5adde0d877d00
Awni <awnyaltabaa@gmail.com>
2024-01-31 01:27:27 -0500

    updated

--- experiments/relational_games/models.py
+++ experiments/relational_games/models.py
@@ -6,7 +6,7 @@ from cnn_embedder import CNNEmbedder
 
 import sys; sys.path.append('..'); sys.path.append('../..')
 from relational_neural_networks.mdipr import MultiDimInnerProdRelation
-from relational_neural_networks.relational_graphlet_convolution import RelationalGraphletConvolution
+from relational_neural_networks.relational_graphlet_convolution import RelationalGraphletConvolution, RelationalGraphletConvolutionGroupAttn
 from relational_neural_networks.grouping_layers import TemporalGrouping, FeatureGrouping
 from relational_neural_networks.tcn import TCN, GroupTCN
 from relational_neural_networks.predinet import PrediNet
@@ -17,11 +17,11 @@ cnn_embedder_kwargs = dict(n_f=(16,16), s_f=(3,3), pool_size=2)
 hidden_dense_size = 64
 
 # RelConvNet
-relconv_mhr_kwargs = dict(rel_dim=16, proj_dim=4, symmetric=True)
-relconv_kwargs = dict(n_filters=16, graphlet_size=3,
-        symmetric_inner_prod=False)
 
 def create_relconvnet(normalizer=None, freeze_embedder=False, object_selection=None):
+    relconv_mhr_kwargs = dict(rel_dim=16, proj_dim=4, symmetric=True)
+    relconv_kwargs = dict(n_filters=16, graphlet_size=3,
+            symmetric_inner_prod=False)
     object_selector = get_obj_selector(object_selection)
     normalizer = get_normalizer(normalizer)
     cnn_embedder = CNNEmbedder(**cnn_embedder_kwargs)
@@ -45,6 +45,36 @@ def create_relconvnet(normalizer=None, freeze_embedder=False, object_selection=N
 
     return model
 
+def create_relconvnet_groupattn():
+    relconv_kwargs = dict(
+        n_filters=16, graphlet_size=3, n_groups=8,
+        mdipr_kwargs=dict(rel_dim=1, proj_dim=16, symmetric=True), # NOTE: changed proj_dim from 4
+        group_attn_key_dim=32, group_attn_key='pos', symmetric_inner_prod=False, permutation_aggregator='max',
+        filter_initializer='random_normal', entropy_reg=True, entropy_reg_scale=0.05)
+    mdipr2_kwargs = dict(rel_dim=8, proj_dim=16, symmetric=True)
+
+    cnn_embedder = CNNEmbedder(**cnn_embedder_kwargs)
+
+    rel_conv1 = RelationalGraphletConvolutionGroupAttn(
+        **relconv_kwargs, name='rgc')
+
+    mdipr2 = MultiDimInnerProdRelation(**mdipr2_kwargs, name='mdipr2')
+    # rel_conv2 = RelationalGraphletConvolutionGroupAttn(
+    #     **relconv_kwargs, name='rgc')
+
+    model = tf.keras.Sequential([
+        cnn_embedder,
+        rel_conv1,
+        mdipr2,
+        tf.keras.layers.Flatten(name='flatten'),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(128, activation='relu', name='hidden_dense1'), # NOTE: changed from hidden_dense_size = 64
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(2, name='output')
+        ], name='relconv'
+    )
+
+    return model
 
 def create_tempgroup_relconvnet(normalizer=None, freeze_embedder=False, object_selection=None):
     class Model(tf.keras.Model):
@@ -250,10 +280,10 @@ def create_predinet(normalizer=None, freeze_embedder=False, object_selection=Non
     return model
 
 ## Transformer
-encoder_kwargs = dict(num_layers=1, num_attention_heads=8, intermediate_size=32,
-    activation='relu', dropout_rate=0.0, attention_dropout_rate=0.0,
-    use_bias=False, norm_first=True, norm_epsilon=1e-06, intermediate_dropout=0.0)
 def create_transformer(normalizer=None, freeze_embedder=False, object_selection=None):
+    encoder_kwargs = dict(num_layers=1, num_attention_heads=8, intermediate_size=32,
+        activation='relu', dropout_rate=0.0, attention_dropout_rate=0.0,
+        use_bias=False, norm_first=True, norm_epsilon=1e-06, intermediate_dropout=0.0)
     object_selector = get_obj_selector(object_selection)
     normalizer = get_normalizer(normalizer)
     cnn_embedder = CNNEmbedder(**cnn_embedder_kwargs)
@@ -272,8 +302,8 @@ def create_transformer(normalizer=None, freeze_embedder=False, object_selection=
     return model
 
 ### Abstractor
-abstractor_kwargs = dict(num_layers=1, num_heads=8, dff=64, use_self_attn=False, dropout_rate=0.)
 def create_abstractor(normalizer=None, freeze_embedder=False, object_selection=None):
+    abstractor_kwargs = dict(num_layers=1, num_heads=8, dff=64, use_self_attn=False, dropout_rate=0.)
     object_selector = get_obj_selector(object_selection)
     normalizer = get_normalizer(normalizer)
     cnn_embedder = CNNEmbedder(**cnn_embedder_kwargs)
@@ -290,6 +320,130 @@ def create_abstractor(normalizer=None, freeze_embedder=False, object_selection=N
         tf.keras.layers.Dense(2)
         ])
     return model
+
+# NOTE: for the models below, normalizer, freeze_embedder, object_selection are ignored...
+# those were used initially for experimentation with models above,
+#  but were later removed from all experiments reported in the paper
+
+# region LSTM
+def create_lstm(normalizer=None, freeze_embedder=False, object_selection=None):
+    lstm_kwargs = dict(units=32, activation='tanh', recurrent_activation='sigmoid', return_sequences=False)
+    cnn_embedder = CNNEmbedder(**cnn_embedder_kwargs)
+    model = tf.keras.Sequential([
+        cnn_embedder,
+        tf.keras.layers.LSTM(**lstm_kwargs),
+        tf.keras.layers.Dense(hidden_dense_size, activation='relu', name='hidden_dense1'),
+        tf.keras.layers.Dense(2, name='output')
+        ])
+    return model
+# endregion
+
+# region LSTM
+def create_gru(normalizer=None, freeze_embedder=False, object_selection=None):
+    gru_kwargs = dict(units=32, activation='tanh', recurrent_activation='sigmoid', return_sequences=False)
+    cnn_embedder = CNNEmbedder(**cnn_embedder_kwargs)
+    model = tf.keras.Sequential([
+        cnn_embedder,
+        tf.keras.layers.GRU(**gru_kwargs),
+        tf.keras.layers.Dense(hidden_dense_size, activation='relu', name='hidden_dense1'),
+        tf.keras.layers.Dense(2, name='output')
+        ])
+    return model
+# endregion
+
+# region GCN
+def create_gcn(normalizer=None, freeze_embedder=False, object_selection=None):
+    gcn_kwargs = dict(channels=32, n_layers=2, dense_dim=32)
+    from spektral.layers import GCNConv
+    class GCNModel(tf.keras.Model):
+        def __init__(self):
+            super().__init__()
+            self.cnn_embedder = CNNEmbedder(**cnn_embedder_kwargs)
+            self.convs = [GCNConv(gcn_kwargs['channels']) for _ in range(gcn_kwargs['n_layers'])]
+            self.denses = [tf.keras.layers.Dense(gcn_kwargs['dense_dim'], activation='relu') for _ in range(gcn_kwargs['n_layers'])]
+            self.pool = tf.keras.layers.GlobalAveragePooling1D()
+            self.hidden_dense1 = tf.keras.layers.Dense(hidden_dense_size, activation='relu', name='hidden_dense1')
+            self.out = tf.keras.layers.Dense(2, name='output')
+
+        def build(self, input_shape):
+            _, self.n_objs, *_ = input_shape
+
+        def call(self, inputs):
+            x = self.cnn_embedder(inputs)
+            a = tf.ones(shape=(tf.shape(x)[0], self.n_objs, self.n_objs))
+            for conv, dense in zip(self.convs, self.denses):
+                x = conv([x, a])
+                x = dense(x)
+            x = self.pool(x)
+            x = self.hidden_dense1(x)
+            out = self.out(x)
+            return out
+
+    return GCNModel()
+# endregion
+
+# region GAT
+def create_gat(normalizer=None, freeze_embedder=False, object_selection=None):
+    gat_kwargs = dict(channels=32, n_layers=2, dense_dim=32)
+    from spektral.layers import GATConv
+    class GATModel(tf.keras.Model):
+        def __init__(self):
+            super().__init__()
+            self.cnn_embedder = CNNEmbedder(**cnn_embedder_kwargs)
+            self.convs = [GATConv(gat_kwargs['channels']) for _ in range(gat_kwargs['n_layers'])]
+            self.denses = [tf.keras.layers.Dense(gat_kwargs['dense_dim'], activation='relu') for _ in range(gat_kwargs['n_layers'])]
+            self.pool = tf.keras.layers.GlobalAveragePooling1D()
+            self.hidden_dense1 = tf.keras.layers.Dense(hidden_dense_size, activation='relu', name='hidden_dense1')
+            self.out = tf.keras.layers.Dense(2, name='output')
+
+        def build(self, input_shape):
+            _, self.n_objs, *_ = input_shape
+
+        def call(self, inputs):
+            x = self.cnn_embedder(inputs)
+            a = tf.ones(shape=(tf.shape(x)[0], self.n_objs, self.n_objs))
+            for conv, dense in zip(self.convs, self.denses):
+                x = conv([x, a])
+                x = dense(x)
+            x = self.pool(x)
+            x = self.hidden_dense1(x)
+            out = self.out(x)
+            return out
+
+    return GATModel()
+# endregion GAT
+
+# region  GIN
+def create_gin(normalizer=None, freeze_embedder=False, object_selection=None):
+    gin_kwargs = dict(channels=32, n_layers=2, dense_dim=32)
+    from spektral.layers import GINConvBatch
+    class GINModel(tf.keras.Model):
+        def __init__(self):
+            super().__init__()
+            self.cnn_embedder = CNNEmbedder(**cnn_embedder_kwargs)
+            self.convs = [GINConvBatch(gin_kwargs['channels']) for _ in range(gin_kwargs['n_layers'])]
+            self.denses = [tf.keras.layers.Dense(gin_kwargs['dense_dim'], activation='relu') for _ in range(gin_kwargs['n_layers'])]
+            self.pool = tf.keras.layers.GlobalAveragePooling1D()
+            self.hidden_dense1 = tf.keras.layers.Dense(hidden_dense_size, activation='relu', name='hidden_dense1')
+            self.out = tf.keras.layers.Dense(2, name='output')
+
+        def build(self, input_shape):
+            _, self.n_objs, *_ = input_shape
+
+        def call(self, inputs):
+            x = self.cnn_embedder(inputs)
+            a = tf.ones(shape=(tf.shape(x)[0], self.n_objs, self.n_objs))
+            for conv, dense in zip(self.convs, self.denses):
+                x = conv([x, a])
+                x = dense(x)
+            x = self.pool(x)
+            x = self.hidden_dense1(x)
+            out = self.out(x)
+            return out
+
+    return GINModel()
+# endregion GIN
+
 
 
 # region utilities for model configurations
@@ -339,5 +493,11 @@ model_creators = dict(
     corelnet=create_corelnet,
     nosoftmax_corelnet=create_nosoftmaxcorelnet,
     predinet=create_predinet,
-    abstractor=create_abstractor
+    abstractor=create_abstractor,
+    lstm=create_lstm,
+    gru=create_gru,
+    gcn=create_gcn,
+    gat=create_gat,
+    gin=create_gin,
+    relconvnet_groupattn=create_relconvnet_groupattn
     )
